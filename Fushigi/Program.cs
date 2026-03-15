6072a05b801402df1db76d5219175c37b0c866e1
Memetendo <datnintendogamer@gmail.com>
2026-03-15 13:18:17 -0400

    changed version number

--- Fushigi/Program.cs
+++ Fushigi/Program.cs
@@ -11,7 +11,7 @@ using FuzzySharp;
 
 internal class Program
 {
-    public const string Version = "v1.8.0 Beta 1";
+    public const string Version = "v1.8.0 Beta 2";
 
     public static MainWindow MainWindow { get; private set; }
 
